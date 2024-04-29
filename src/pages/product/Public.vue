39b57eb8cc1e3366697bc8e63c69e5d9bd047d91
Pedro Mota <94080428+pedromota05@users.noreply.github.com>
2024-04-29 00:23:33 -0300

    Update Public.vue

--- src/pages/product/Public.vue
+++ src/pages/product/Public.vue
@@ -1,17 +1,17 @@
 <template>
   <q-header elevated>
     <a href="/" style="text-decoration: none">
-        <q-toolbar class="row no-wrap items-center" style="height: 72px; padding-left: 16px;">
-	    <q-img class="doc-header__logo-img" src="~assets/logo-dark.svg" alt="Quasar Logo" style="width: 48px; height: 48px;" />
-	    <q-toolbar-title style="color: #fff; font-weight: 500; letter-spacing: 1px;" v-if="isDesktop">
-	        {{ brand.name }}
-	    </q-toolbar-title>
-	    <q-input rounded outlined dense debounce="300" v-model="filter" placeholder="Search" class="q-mr-sm col-11" style="margin: 20px 0" v-if="isMobile">
-	        <template v-slot:append>
-	            <q-icon name="mdi-magnify"></q-icon>
-	        </template>
-	    </q-input>
-	</q-toolbar>
+      <q-toolbar class="row no-wrap items-center" style="height: 72px; padding-left: 16px;">
+	<q-img class="doc-header__logo-img" src="~assets/logo-dark.svg" alt="Quasar Logo" style="width: 48px; height: 48px;" />
+	<q-toolbar-title style="color: #fff; font-weight: 500; letter-spacing: 1px;" v-if="isDesktop">
+	  {{ brand.name }}
+	</q-toolbar-title>
+	<q-input rounded outlined dense debounce="300" v-model="filter" placeholder="Search" class="q-mr-sm col-11" style="margin: 20px 0" v-if="isMobile">
+	  <template v-slot:append>
+	    <q-icon name="mdi-magnify"></q-icon>
+	  </template>
+	</q-input>
+      </q-toolbar>
     </a>
   </q-header>
   <q-page style="padding: 30px">
