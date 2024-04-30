3c25d1db37a0856e788cf79686c23fab8d58ee8e
Pedro Mota <94080428+pedromota05@users.noreply.github.com>
2024-04-30 08:55:55 -0400

    Update Public.vue

--- src/pages/product/Public.vue
+++ src/pages/product/Public.vue
@@ -25,7 +25,6 @@
 	:autoplay="3000"
 	@mouseenter="autoplay = false"
 	@mouseleave="autoplay = true"
-	height="420"
 	v-if="isDesktop"
 	>
             <img :name="1" src="https://img.freepik.com/psd-premium/banner-de-midia-social-semana-do-consumidor-com-40-de-desconto_318354-384.jpg?w=826" style="background-position: center; background-repeat: no-repeat; width: 100%; height: 100%; position: absolute;" />
